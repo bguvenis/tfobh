759d808a9be3343bdd11d21708453688ebb56941
Baris Guvenis <bguvenis@gmail.com>
2012-01-10 15:58:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tfobh::Application.config.secret_token = 'abe97753cfec1303185c369e470007ba4801bde89c04ede43d5a335688197a33bf482903e52be8572a1e41912c18a8a8d3386c5f3b43405f1e85ff3e2b8457a0'
